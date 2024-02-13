3040f2b04996b6a6fbca07e9f08ebb1c3ca9c8a8
PrahladMehta <pankajmehta4561@gmail.com>
2024-02-13 23:23:38 +0530

    deploying

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      gridTemplateColumns: {
+        
+        'footer': '400px repeat(3,1fr) ',
+      },
+
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue:'#02042a',
+        lightBlue:'#2b84ed',
+        lightBlue300:'#4b94ed',
+        lightBlue500:'#0b72e7',
+        greenLight:'#61cea6',
+        lightGray:'#e2e2e2',
+        grayBlue:'#344a6c',
+        deepBlueHead:'#162f56',
+        gray2:'#525a76', 
+      },
+      },
+    },
+ 
+  plugins: [], 
+
+}
